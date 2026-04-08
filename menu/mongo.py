50f518e30b0c4fc28edf291a093909ae24f2dd8c
Novaxyn <donovanaurelius16@gmail.com>
2026-04-08 21:47:32 +0700

    delete mongo file

--- menu/mongo.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://nopan_db:lilo123@cluster0.unbki7s.mongodb.net/?appName=Cluster0")
-db = client["quickbite_db"]
-menu_collection = db["menus"]
